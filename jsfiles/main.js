4f53debd754dc12587e4399c560a48abbe4e2b2d
Praveen Kumar Guntoju <gpravin999@gmail.com>
2018-12-06 22:36:58 +0000

    Add files via upload

---
+++ jsfiles/main.js
@@ -0,0 +1,50 @@
+function signUpForm() {
+  document.getElementsByClassName("registerForm")[0].style.display = "block";
+  document.getElementsByClassName("login")[0].style.display = "none";
+}
+
+function loginForm() {
+  document.getElementsByClassName("login")[0].style.display = "block";
+  document.getElementsByClassName("registerForm")[0].style.display = "none";
+}
+
+function login() {
+  debugger;
+  var data = {};
+  data.name = document.getElementById("username1").value;
+  data.password = document.getElementById("password1").value;
+  $.ajax({
+    type: "POST",
+    data: JSON.stringify(data),
+    contentType: "application/json",
+    url: "/login",
+    success: function(data) {
+      console.log("success");
+      console.log(JSON.stringify(data));
+    }
+  });
+}
+
+function register() {
+  var data = {
+    email: document.getElementById("email").value,
+    fname: document.getElementById("fname").value,
+    lname: document.getElementById("lname").value,
+    address: document.getElementById("address").value,
+    city: document.getElementById("city").value,
+    state: document.getElementById("state").value,
+    postcode: document.getElementById("postcode").value,
+    mobile: document.getElementById("mobile").value,
+    password: document.getElementById("password").value
+  };
+  $.ajax({
+    type: "POST",
+    data: JSON.stringify(data),
+    contentType: "application/json",
+    url: "/registeruser",
+    success: function(data) {
+      console.log("success");
+      console.log(JSON.stringify(data));
+    }
+  });
+}
---
+++ server.js
@@ -0,0 +1,72 @@
+var express = require("express");
+var app = require("express")();
+var http = require("http").Server(app);
+var fs = require("fs");
+var bodyParser = require("body-parser");
+var path = require("path");
+var mongodb = require("mongodb");
+var dataBase;
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.static(__dirname + "/images"));
+
+app.use(express.static(__dirname + "/aframefiles"));
+app.use(express.static(__dirname + "/3dmodels"));
+// app.use(express.static(__dirname + "/toon_dinosaur_creature_3"));
+app.use(express.static(__dirname + "/jsfiles"));
+app.use(express.static(__dirname + "/patterns"));
+app.use(express.static(__dirname + "/styles"));
+app.use(express.static(__dirname + "/phpfiles"));
+
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.post("/login", function(req, res) {
+  var newContact = req.body.name;
+  console.log(newContact);
+  console.log(dataBase);
+  dataBase
+    .collection("customers")
+    .findOne({ email: newContact }, function(err, doc) {
+      if (err) {
+        console.log("ERROR: " + reason);
+        res.status(code || 500).json({ error: message });
+      } else {
+        res.status(201).json(doc.ops[0]);
+      }
+    });
+});
+var MongoClient = require("mongodb").MongoClient;
+var url = "mongodb://praveen:chintu123@ds161210.mlab.com:61210/shopping";
+
+MongoClient.connect(
+  url,
+  function(err, db) {
+    if (err) throw err;
+
+    dataBase = db;
+  }
+);
+
+app.post("/registeruser", function(req, res) {
+  var newContact = req.body;
+  console.log(newContact);
+  console.log(dataBase);
+  dataBase.collection("customers").insertOne(newContact, function(err, doc) {
+    if (err) {
+      console.log("ERROR: " + reason);
+      res.status(code || 500).json({ error: message });
+    } else {
+      res.status(201).json(doc.ops[0]);
+      res.send("success");
+    }
+  });
+});
+
+// Initialize the app.
+var server = app.listen(process.env.PORT || 8080, function() {
+  var port = server.address().port;
+  console.log("App now running on port", port);
+});
