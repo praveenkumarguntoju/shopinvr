5942e4c6a9084c3d6b65057ca4c30c052de66f1d
Praveen Kumar Guntoju <gpravin999@gmail.com>
2018-12-07 00:17:18 +0000

    Update server.js

--- server.js
+++ server.js
@@ -59,7 +59,6 @@ app.post("/registeruser", function(req, res) {
       console.log("ERROR: " + reason);
       res.status(code || 500).json({ error: message });
     } else {
-      res.status(201).json(doc.ops[0]);
       res.send("success");
     }
   });
